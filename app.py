5958af8a59f5a732c8ca33ec431203c30b4b72ea
sxpistols <mediokers@yahoo.com>
2017-10-22 23:44:20 +0700

    3rd commited

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 from flask import Flask
+from flask import request
+from flask import make_response
+from flask import redirect
+
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Hello World'
+    user_agent = request.headers.get('User-Agent')
+    return 'Hello World, %s <br>' % user_agent
 
 @app.route('/route/<name>')
 def route(name):
     return '<h1>Hi, %s </h1>' % name
 
+@app.route('/response')
+def response():
+    response = make_response('<h1>This doc carries a cookie!</h1>')
+    response.set_cookie('answer','42');
+    return response
+
+
+@app.route('/redirecxt')
+def redirecxt():
+    return redirect('http://www.google.com')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
