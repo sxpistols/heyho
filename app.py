05ddbb30ea441095a24994a79b7bfa5df86a2538
sxpistols <mediokers@yahoo.com>
2017-10-22 23:13:10 +0700

    3rd commited

--- app.py
+++ app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 def index():
     return 'Hello World'
 
+@app.route('/route/<name>')
+def route(name):
+    return '<h1>Hi, %s </h1>' % name
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
